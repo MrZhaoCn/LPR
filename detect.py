6dd58cac4ae2e2bcfaed46b9d8d2c2173bd829b7
zhaocun <zhaocun@youzan.com>
2018-10-18 13:01:45 +0800

    cropImage

--- detect.py
+++ detect.py
@@ -7,18 +7,16 @@ Created on Tue Oct 16 22:13:54 2018
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-imagePath = "./change_size/2.jpg"
-img = cv2.imread(imagePath)
-
-#China car plate size: 440mm*140mm，aspect 3.142857
+imagePath = "./change_size/344.jpg"
+#China car plate size: 440mm*140mm，aspect 3.142857 136,36
 aspect = 3.142857
-errorRate = 0.2
-minArea = 44 * 14 * 0.2
+errorRate = 0.5
+minArea = 44 * 14 * 0.4
 maxArea = 44 * 14 * 10
-rateMin = aspect - aspect * errorRate#最小宽高比
+rateMin = aspect - aspect * errorRate * errorRate #最小宽高比
 rateMax = aspect + aspect * errorRate #最大宽高比
-
-
+maxAngle = 20
+minAngle = -20
 
 def isUsefulContour(contour):
     rect = cv2.minAreaRect(contour)
@@ -26,51 +24,76 @@ def isUsefulContour(contour):
     if width <= 0 or height <= 0:
         return False,rect
     rate = float(width) / height
+    angle = rect[2]
     if rate < 1 :
         rate = float(height) / width
+        angle = angle + 90
     if (rate >= rateMin) and (rate <= rateMax):
         area = width * height
         if area < minArea or area > maxArea:
             return False,rect
-        angle = rect[2]
-        if angle > -30:
-            print(rect[2])
+        if angle >=0 and angle <= maxAngle:
+            return True,rect
+        
+        if angle <=0 and angle >= minAngle:
             return True,rect
     return False,rect
+
+def detectCarPlate(imagePath):
+    img = cv2.imread(imagePath)
+    #高斯平滑
+    gaussion = cv2.GaussianBlur(img,(3,3),0,0,cv2.BORDER_DEFAULT)
+    #中值滤波
+    #median = cv2.medianBlur(gaussion,5)
+    #灰度化
+    grayImage = cv2.cvtColor(gaussion,cv2.COLOR_BGR2GRAY)
+    #sobel算子
+    x = cv2.Sobel(grayImage,cv2.CV_8U,1,0,ksize=3)  
+    y = cv2.Sobel(grayImage,cv2.CV_8U,0,1,ksize=3)  
+    absX = cv2.convertScaleAbs(x)
+    absY = cv2.convertScaleAbs(y)
+    sobel = cv2.addWeighted(absX,0.6,absY,0.6,0)
     
-#高斯平滑
-gaussion = cv2.GaussianBlur(img,(5,5),0,0,cv2.BORDER_DEFAULT)
-#中值滤波
-#median = cv2.medianBlur(gaussion,5)
-#灰度化
-grayImage = cv2.cvtColor(gaussion,cv2.COLOR_BGR2GRAY)
-#sobel算子
-x = cv2.Sobel(grayImage,cv2.CV_8U,1,0,ksize=3)  
-y = cv2.Sobel(grayImage,cv2.CV_8U,0,1,ksize=3)  
-absX = cv2.convertScaleAbs(x)
-absY = cv2.convertScaleAbs(y)
-sobel = cv2.addWeighted(absX,0.5,absY,0.5,0)
-
-#二值化
-ret,binary = cv2.threshold(sobel,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU )
-
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(20,3))
-
-colose = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)  # 闭运算
-
-#轮廓截取
-_ ,contours,_ = cv2.findContours(colose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-userfulContours = []
-Box = []
-for contour in contours: 
-    userful,rect = isUsefulContour(contour)
-    box = np.int0(cv2.boxPoints(rect))
-    Box.append(box)
-    if userful:
-        userfulContours.append(contour)
+    #二值化
+    ret,binary = cv2.threshold(sobel,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU )
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(12,3))
+    
+    colose = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)  # 闭运算
+    # perform a series of erosions and dilations
+    #closed = cv2.erode(closed, None, iterations=4)
+    #closed = cv2.dilate(closed, None, iterations=4)
+    #轮廓截取
+    _ ,contours,_ = cv2.findContours(colose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    userfulContours = []
+    Box = []
+    cropImages = []
+    i = 0
+    for contour in contours: 
+        userful,rect = isUsefulContour(contour)
         #box = np.int0(cv2.boxPoints(rect))
         #Box.append(box)
+        if userful:
+            i = i + 1
+            userfulContours.append(contour)
+            box = np.int0(cv2.boxPoints(rect))
+            # step7：裁剪。box里保存的是绿色矩形区域四个顶点的坐标。我将按下图红色矩形所示裁剪昆虫图像。
+            # 找出四个顶点的x，y坐标的最大最小值。新图像的高=maxY-minY，宽=maxX-minX。
+            Xs = [i[0] for i in box]
+            Ys = [i[1] for i in box]
+            x1 = min(Xs)
+            x2 = max(Xs)
+            y1 = min(Ys)
+            y2 = max(Ys)
+            hight = y2 - y1
+            width = x2 - x1
+            cropImg = img[y1:y1+hight, x1:x1+width]
+            cv2.imwrite(imagePath+ str(i) + '.jpg',cropImg)
+            cropImages.append(cropImg)
+            #Box.append(box)
         
-imag = cv2.drawContours(img.copy(),Box,-1,(0,0,255),1)
-cv2.imshow("imag",imag)
-cv2.waitKey(1000) 
+# =============================================================================
+#     imag = cv2.drawContours(img.copy(),Box,-1,(0,0,255),1)
+#     cv2.imshow("imag",imag)
+#     cv2.waitKey(1000) 
+# =============================================================================
--- imageUtil.py
+++ imageUtil.py
@@ -8,26 +8,40 @@ Created on Wed Oct 17 22:19:25 2018
 
 import cv2
 import os
+import detect
 image_size = 900
 source_path = "./images/"
 target_path = "./change_size/"
-if not os.path.exists(target_path):
-    os.makedirs(target_path)
-image_list = os.listdir(source_path)
-i = 0
-for file in image_list:
-    i = i+ 1
-    image_soure = cv2.imread(source_path+file)
-    if image_soure is None:
-        continue
-    if image_soure.any() == False:
-        continue
-    (height,width) = image_soure.shape[:2]
-    
-    if width > image_size:
-        rate = float(width) / height
-        resize_height =  int(image_size / rate)
-        image = cv2.resize(image_soure,(image_size,resize_height),0,0,cv2.INTER_LINEAR)
-        cv2.imwrite(target_path+str(i)+".jpg",image)
+
+def resizeImages():
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+    image_list = os.listdir(source_path)
+    i = 0
+    for file in image_list:
+        i = i+ 1
+        image_soure = cv2.imread(source_path+file)
+        if image_soure is None:
+            continue
+        if image_soure.any() == False:
+            continue
+        (height,width) = image_soure.shape[:2]
         
+        if width > image_size:
+            rate = float(width) / height
+            resize_height =  int(image_size / rate)
+            image = cv2.resize(image_soure,(image_size,resize_height),0,0,cv2.INTER_LINEAR)
+            cv2.imwrite(target_path+str(i)+".jpg",image)
+
+def detectCarPlates():
+    image_list = os.listdir(target_path)
+    for file in image_list:
+      detectCarPlate(target_path+file)
+      print(target_path+file)
+        
+if __name__ == "__main__":
+    detectCarPlates()
+    
+    
+            
         
